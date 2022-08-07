81d8e2e9aa4c79921b476780bd3a3bb700e7e979
kpxb <coolapk.kpxb@gmail.com>
2022-08-07 13:39:59 +0000

    First commit

---
+++ sources/uart-test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define uart5 "/dev/ttyS5"
+
+int main(int argc, char *argv[])
+{
+	FILE *u_fd;
+	char buff[512];
+	
+	printf("Opening uart5 ...\n");
+
+	u_fd = fopen(uart5, "r+");
+	if(u_fd < 0)
+	{
+      		printf("Fail to Open uart5\n");
+     		exit(1);
+   	}
+	else
+	{
+		printf("Success to Open uart5\n");
+	}
+
+	while(1)
+	{
+		if(fread(buff, 512, 1, u_fd))
+		{
+			fwrite(buff, 512, 1, u_fd);
+		}
+
+	}
+}	
